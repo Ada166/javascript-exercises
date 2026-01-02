164278064accb720f0b1a87e9c175df96ae6ccf9
Ada <xandrewsteam@gmail.com>
2026-01-02 21:44:27 +0100

    Expects Error message for some terms,if Statements need adjusted

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,14 +1,16 @@
-const sumAll = function(sumAll) {
-    let start = Math.min(start, end)
-    let end = Math.max(start, end);
+const sumAll = function(start, end) {
+    start = Math.min(start, end) * 1;
+    end = Math.max(start, end) * 1;
+    if (typeof start !== 'number' || typeof end !== 'number') {
+        return 'ERROR';
+    }
     if (Number.isInteger(start) && Number.isInteger(end) && start >= 0 && end >= 0) {
-        sumAll = 0;
+        let sumAll = 0;
         for (let i = start; i <= end; i++) {
             sumAll += i;
         }
         return sumAll;
-    }
-
+    } 
 };
 
 module.exports = sumAll;
