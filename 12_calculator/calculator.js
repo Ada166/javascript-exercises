89ee65af1bf83781bd31b02825e26f4be664d22e
Ada <xandrewsteam@gmail.com>
2026-02-21 16:41:29 +0100

    Solved all exercises from calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -22,12 +22,21 @@ const multiply = function(arr) {
   return multiply;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	let result = 1;
+  for (let i = 0; i < exp; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
