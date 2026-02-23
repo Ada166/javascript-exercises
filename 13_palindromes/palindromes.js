23c21b1f98a12efb6989cf3c0bfb9f68af91f8d0
Ada <xandrewsteam@gmail.com>
2026-02-23 21:43:12 +0100

    Solved palindrome exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleaned = string;
+    cleaned = cleaned.toLowerCase();
+    cleaned = cleaned.replace(/\W/g, "");
+    let reversed = cleaned.split("").reverse().join("");
+    for (let i=0; i<string.length; i+=1) {
+        if (cleaned !== reversed) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// Write a for loop iterating thru each string letter and if the same then return true
+// Needs to accept uppercase, numbers and unevenly spaced numbers
