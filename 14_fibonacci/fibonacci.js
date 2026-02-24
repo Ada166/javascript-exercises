e1c5d27a1d4a0a3458693a35b3c2efe9a9e9c152
Ada <xandrewsteam@gmail.com>
2026-02-24 22:30:11 +0100

    Stuck at accepting strings for fibonacci numbers WIP

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof n2 === 'string') {
+        return n2;
+    }
+    let n1 = 0;
+    let n2 = 1;
+    let sum;
+    if (num === 1) {
+        return n1;
+    } else if (num === 2) {
+        return n2;
+    } else {
+        for (let i = 2; i <= num; i++) {
+            sum = n1 + n2;
+            n1 = n2;
+            n2 = sum;
+        }
+        return n2;
+    }
 };
 
 // Do not edit below this line
