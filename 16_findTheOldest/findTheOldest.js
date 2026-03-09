08ceed71581011d6f48711147dd4196d3115ff58
Ada <xandrewsteam@gmail.com>
2026-03-09 22:36:34 +0100

    Solved findTheOldest with loop, will try also with reduce method

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
 const findTheOldest = function(people) {
-    
+    let currentYear = new Date().getFullYear();
+    let oldest = people[0];
+    for (let i = 1; i < people.length; i++) {
+        let person = people[i];
+        let oldestAge;
+        if (oldest.yearOfDeath) {
+            oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        } else {
+            oldestAge = currentYear - oldest.yearOfBirth;
+        }
+        let currentAge;
+        if (person.yearOfDeath) {
+            currentAge = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            currentAge = currentYear - person.yearOfBirth;
+        }
+        if (currentAge > oldestAge) {
+            oldest = person;
+        }
+    }
+    return oldest;
 }
 // Do not edit below this line
 module.exports = findTheOldest;
